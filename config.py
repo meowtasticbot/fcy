626659377c46567a2ad4fe883b2bac3e24083301
meowtasticbot <moonop615@gmail.com>
2026-02-26 16:28:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 SOURCE = getenv("SOURCE", "https://t.me/Meowstric")
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/abcdefghij803/Clonify",
+    "https://github.com/meowtasticbot/fcy",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
